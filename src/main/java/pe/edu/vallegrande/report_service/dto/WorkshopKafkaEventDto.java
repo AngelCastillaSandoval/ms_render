b6ac6d59368548b8f1a56489c33e467d48221c8b
AngelCastillaSandoval <127228998+AngelCastillaSandoval@users.noreply.github.com>
2025-05-01 21:55:18 -0500

    Update WorkshopKafkaEventDto.java

--- src/main/java/pe/edu/vallegrande/report_service/dto/WorkshopKafkaEventDto.java
+++ src/main/java/pe/edu/vallegrande/report_service/dto/WorkshopKafkaEventDto.java
@@ -1,5 +1,6 @@
 package pe.edu.vallegrande.report_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,7 +9,13 @@ import java.time.LocalDate;
 public class WorkshopKafkaEventDto {
     private Integer id;
     private String name;
+
+    @JsonProperty("startDate")
     private LocalDate dateStart;
+
+    @JsonProperty("endDate")
     private LocalDate dateEnd;
+
+    @JsonProperty("state")
     private String status;
 }
