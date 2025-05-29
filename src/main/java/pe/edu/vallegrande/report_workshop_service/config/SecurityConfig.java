f35584e5bccf0343b0b443d8c5b68aab512083e4
AngelCastillaSandoval <127228998+AngelCastillaSandoval@users.noreply.github.com>
2025-05-29 02:39:04 -0500

    Update SecurityConfig.java

--- src/main/java/pe/edu/vallegrande/report_workshop_service/config/SecurityConfig.java
+++ src/main/java/pe/edu/vallegrande/report_workshop_service/config/SecurityConfig.java
@@ -6,10 +6,13 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
 
-import java.util.List;
-
+/**
+ * Configura la seguridad del microservicio:
+ * - Permite acceso sin autenticación a Swagger y Actuator
+ * - Requiere JWT para acceder al resto de los endpoints
+ * - Cors
+ */
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -19,22 +22,23 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(auth -> auth
+                        .pathMatchers("/swagger-ui.html").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(Customizer.withDefaults())
                 )
-                .cors(cors -> cors.configurationSource(exchange -> {
-                    CorsConfiguration config = new CorsConfiguration();
-                    config.setAllowCredentials(true);
-                    config.setAllowedOriginPatterns(List.of(
-                            "http://localhost:4200",
-                            "https://frontend-prs1.onrender.com"
-                    ));
-                    config.addAllowedHeader("*");
-                    config.addAllowedMethod("*");
-                    return config;
-                }))
+                .cors(cors -> cors
+                        .configurationSource(exchange -> {
+                            var config = new org.springframework.web.cors.CorsConfiguration();
+                            config.setAllowCredentials(true);
+                            // Especifica el origen permitido
+                            config.addAllowedOrigin("http://localhost:4200");
+                            config.addAllowedHeader("*");
+                            config.addAllowedMethod("*");
+                            return config;
+                        })
+                )
                 .build();
     }
 }
